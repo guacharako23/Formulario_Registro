d1639b935463bddb5c19a8e9b1f0aa918757abc1
Ronald_Barraza <87039930+guacharako23@users.noreply.github.com>
2023-04-19 20:36:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let tabla=[
+    {nombre:'Luis' ,apellido: 'Martinez' ,direccion:'Calle 13 N°15 -25' ,correo_electronico: 'efe@gmail.com' ,telefono: '331123456790' ,ciudad: 'Barranquilla' ,departamento: 'Atlantico',codigopostal: '0451112'},
+    {nombre:'Ronald' ,apellido: 'Barraza' ,direccion:'Calle 41 N°17C -185' ,correo_electronico: 'supervisor@gmail.com' ,telefono: '3216957814' ,ciudad: 'Medellin' ,departamento: 'Antioquia' ,codigopostal: '12345678'},
+    {nombre:'Enrique' ,apellido: 'Montes' ,direccion:'Calle 38 N°78 -25' ,correo_electronico: 'otrapersona@gmail.com' ,telefono: '331414557' ,ciudad: 'Bogota' ,departamento: 'Cundinamarca' ,codigopostal: '987654321'}
+]
+let cuerpotabla = document.getElementById('tbody');
+let a = 1;
+const listar=()=>{
+    let usuarios ="";
+    for (let i = 0; i < tabla.length; i++){
+        usuarios += `<tr><th scope="row">${i+1}</th><td>${tabla[i].nombre}</td><td>
+        ${tabla[i].apellido}</td><td>${tabla[i].departamento}</td><td>${tabla[i].correo_electronico}</td><td>${tabla[i].telefono}</td><td>
+        ${tabla[i].ciudad}</td><td>${tabla[i].direccion}</td><td>${tabla[i].codigopostal}</td></tr>`;
+    }
+  cuerpotabla.innerHTML = usuarios;
+}
+
+const registrar=()=>{
+    const nombre = document.getElementById('nombre').value ;
+    const apellido = document.getElementById('apellido').value ;
+    const direccion = document.getElementById('direccion').value ;
+    const correo_electronico = document.getElementById('correo_electronico').value ;
+    const telefono = document.getElementById('telefono').value ;
+    const ciudad = document.getElementById('ciudad').value ;
+    const departamento = document.getElementById('departamento').value ;
+    const codigopostal = document.getElementById('codigopostal').value ;
+    
+    cuerpotabla.innerHTML += `<tr><th scope="row">${a}</th><td>${nombre}</td><td>${apellido}</td><td>${direccion}</td>
+    <td>${correo_electronico}</td><td>${telefono}</td><td>${ciudad}</td><td>${departamento}</td><td>${codigopostal}</td></tr>`; 
+    const nuevo = {nombre,apellido,direccion,correo_electronico,telefono,ciudad,departamento,codigopostal};
+    tabla.push(nuevo);
+    
+}
+
+
